52a92bd02626bb6e6483023338b292a43f132565
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-04-09 07:35:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+let text = confirm("Do you want to proceed?");
+
+if(text == true){
+	alert("You clicked OK. Proceeding...")
+}else{
+	alert("You clicked Cancel. Exiting...")
+}
+
+
+
